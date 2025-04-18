99dbdc2f72ca2d7995ef0e3e968a87d367c1e065
Enzo Fernandes <enzofernandes@usp.br>
2025-04-18 17:37:25 +0300

    [feat] home page criada

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import HomePage from './pages/HomePage';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
       </Routes>
     </Router>
